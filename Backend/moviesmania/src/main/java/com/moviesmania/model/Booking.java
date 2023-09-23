7243da297842008d19992aedf40688c46c68fc7c
Deependra Yadav <deependrayadav6306@gmail.com>
2023-09-23 17:55:57 +0530

    Booking class and BookingStatus enum created inside model package

---
+++ Backend/moviesmania/src/main/java/com/moviesmania/model/Booking.java
@@ -0,0 +1,31 @@
+package com.moviesmania.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Booking {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer bookingNumber;
+	
+	private Integer numberOfSeats;
+	
+	private LocalDateTime bookingTime = LocalDateTime.now();
+	
+	private BookingStatus bookingStatus;
+	
+	
+
+	@OneToOne
+	private Payment payment;
+	
+}
