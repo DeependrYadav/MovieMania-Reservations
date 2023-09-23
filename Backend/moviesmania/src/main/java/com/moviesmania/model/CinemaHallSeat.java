f6540aa45134c7666ef80b3d299ed98bde34a948
Deependra Yadav <deependrayadav6306@gmail.com>
2023-09-24 02:07:37 +0530

    Data validation is implemented in CinemaHallSeat class

--- Backend/moviesmania/src/main/java/com/moviesmania/model/CinemaHallSeat.java
+++ Backend/moviesmania/src/main/java/com/moviesmania/model/CinemaHallSeat.java
@@ -1,6 +1,10 @@
 package com.moviesmania.model;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +15,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CinemaHallSeat {
 
+	@NotBlank(message = "Provide Seat name.")
 	public String seatName;
+	
 	private boolean isReserved;
+	@NotNull(message = "Provide price of seat.")
 	private Double price;
+	
+	@Enumerated(EnumType.STRING)
 	private SeatType seatType;
 }
