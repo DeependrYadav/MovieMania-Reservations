67c362a52a072772df39b4c8178cb1075262d479
Deependra Yadav <deependrayadav6306@gmail.com>
2023-09-23 11:09:09 +0530

    Spring project created

---
+++ Backend/moviesmania/moviesmania/src/test/java/com/moviesmania/MoviesmaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviesmania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesmaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
