a58581ff3f1d340a209b2d2f6cfb112b40e6291d
Deependra Yadav <deependrayadav6306@gmail.com>
2023-09-24 14:49:45 +0530

    JWT authentication is implemented

---
+++ Backend/moviesmania/src/main/java/com/moviesmania/config/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.moviesmania.config;
+
+public interface SecurityConstants {
+	
+	public static final String JWT_KEY ="secretsfhsfjhdkjngdfjkgfgjdlkfjsdkfjsd";
+	public static final String JWT_HEADER = "Authorization";
+
+}
