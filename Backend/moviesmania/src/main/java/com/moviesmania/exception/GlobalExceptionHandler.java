fd6a9fa40b265a49ef0fd0a42d0050bad35fe0d0
Deependra Yadav <deependrayadav6306@gmail.com>
2023-09-26 09:02:35 +0530

    MoviesManiaException and NoHandlerFoundException handled in GlobalExceptionHandler

--- Backend/moviesmania/src/main/java/com/moviesmania/exception/GlobalExceptionHandler.java
+++ Backend/moviesmania/src/main/java/com/moviesmania/exception/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,6 +16,19 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<MyExceptionClass> exception(Exception ex, WebRequest web){
 		MyExceptionClass mec = new MyExceptionClass(ex.getMessage(), web.getDescription(false), LocalDateTime.now());
+		return new ResponseEntity<MyExceptionClass>(mec,HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(MoviesManiaException.class)
+	public ResponseEntity<MyExceptionClass> exception(MoviesManiaException ex, WebRequest web){
+		MyExceptionClass mec = new MyExceptionClass(ex.getMessage(), web.getDescription(false), LocalDateTime.now());
+		return new ResponseEntity<MyExceptionClass>(mec,HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<MyExceptionClass> myException(NoHandlerFoundException ex,WebRequest web){
+		MyExceptionClass mec = new MyExceptionClass(ex.getMessage(), web.getDescription(false), LocalDateTime.now());
+		
 		return new ResponseEntity<MyExceptionClass>(mec,HttpStatus.BAD_REQUEST);
 	}
 }
