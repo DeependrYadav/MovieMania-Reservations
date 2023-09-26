b58a71ba20eda105fb1e97fdc57d5ee555850c66
Deependra Yadav <deependrayadav6306@gmail.com>
2023-09-26 09:03:58 +0530

    MethodArgumentNotValidException for input validation handled in GlobalExceptionHandler

--- Backend/moviesmania/src/main/java/com/moviesmania/exception/GlobalExceptionHandler.java
+++ Backend/moviesmania/src/main/java/com/moviesmania/exception/GlobalExceptionHandler.java
@@ -1,13 +1,17 @@
 package com.moviesmania.exception;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
+
 
 
 @ControllerAdvice
@@ -28,6 +32,20 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(NoHandlerFoundException.class)
 	public ResponseEntity<MyExceptionClass> myException(NoHandlerFoundException ex,WebRequest web){
 		MyExceptionClass mec = new MyExceptionClass(ex.getMessage(), web.getDescription(false), LocalDateTime.now());
+		
+		return new ResponseEntity<MyExceptionClass>(mec,HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<MyExceptionClass> myException(MethodArgumentNotValidException ex,WebRequest web){
+		MyExceptionClass mec = new MyExceptionClass("Valiation Failed", web.getDescription(false), LocalDateTime.now());
+		
+		List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+		List<String> errorsToStringList = MethodArgumentNotValidException.errorsToStringList(allErrors);
+		
+		String str = String.join(",",errorsToStringList);
+		
+		mec.setDescription(str);
 		
 		return new ResponseEntity<MyExceptionClass>(mec,HttpStatus.BAD_REQUEST);
 	}
