6b86d4f1bb9c6e747796258af51e7678886773df
Deependra Yadav <deependrayadav6306@gmail.com>
2023-09-26 08:59:36 +0530

    Exception class handled in GlobalExceptionHandler

--- Backend/moviesmania/src/main/java/com/moviesmania/exception/GlobalExceptionHandler.java
+++ Backend/moviesmania/src/main/java/com/moviesmania/exception/GlobalExceptionHandler.java
@@ -1,8 +1,19 @@
 package com.moviesmania.exception;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<MyExceptionClass> exception(Exception ex, WebRequest web){
+		MyExceptionClass mec = new MyExceptionClass(ex.getMessage(), web.getDescription(false), LocalDateTime.now());
+		return new ResponseEntity<MyExceptionClass>(mec,HttpStatus.BAD_REQUEST);
+	}
 }
