b3dbb6ebf8143725bf17e57d9c42fa532f2b91e7
Deependra Yadav <deependrayadav6306@gmail.com>
2023-09-27 21:39:57 +0530

    mordify show method implemented

--- Backend/moviesmania/src/main/java/com/moviesmania/service/CinemaHallServiceImpl.java
+++ Backend/moviesmania/src/main/java/com/moviesmania/service/CinemaHallServiceImpl.java
@@ -1,6 +1,11 @@
 package com.moviesmania.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.moviesmania.exception.MoviesManiaException;
@@ -48,6 +53,27 @@ public class CinemaHallServiceImpl implements CinemaHallService{
 		
 		
 		return chr.save(cinemaHall);
+	}
+
+	@Override
+	public List<CinemaHall> viewCinemaHall(Integer pagination, Integer records, String sortBy, String direction) {
+		Pageable pageable = null;
+		Sort sort = null;
+		if(pagination != null && records == null)throw new MoviesManiaException("Provide value of records per page");
+		if(sortBy != null && direction == null)throw new MoviesManiaException("Provide value of sorting direction");
+			
+		if(pagination != null && sortBy != null) {
+			sort = Sort.by((direction.equalsIgnoreCase("asc")?"ASC":"DESC"), sortBy);
+			pageable = PageRequest.of(pagination, records,sort);
+			return chr.findAll(pageable).toList();
+		}else if(pagination != null) {
+			pageable = PageRequest.of(pagination, records);
+			return chr.findAll(pageable).toList();
+		}else if(sortBy != null) {
+			sort = Sort.by((direction.equalsIgnoreCase("asc")?"ASC":"DESC"), sortBy);
+			return chr.findAll(sort);
+		}
+		return chr.findAll();
 	}
 
 }
--- Backend/moviesmania/src/main/java/com/moviesmania/service/ShowServiceImpl.java
+++ Backend/moviesmania/src/main/java/com/moviesmania/service/ShowServiceImpl.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.moviesmania.exception.MoviesManiaException;
+import com.moviesmania.model.CinemaHall;
 import com.moviesmania.model.Movie;
 import com.moviesmania.model.MovieShow;
+import com.moviesmania.repository.CinemaHallRepository;
 import com.moviesmania.repository.MoviesRepository;
 import com.moviesmania.repository.ShowRepository;
 
@@ -19,17 +21,68 @@ public class ShowServiceImpl implements ShowService{
 	@Autowired
 	private ShowRepository sr;
 	
+	@Autowired
+	private CinemaHallRepository chr;
+	
 	@Override
 	public MovieShow addShow(Integer movieId,Integer cinemaHallId,@Valid MovieShow show) {
 		
 		Movie movie = mr.findById(movieId).orElseThrow(()-> new MoviesManiaException("Invalid movie ID"));
 		
+		CinemaHall ch = chr.findById(cinemaHallId).orElseThrow(()-> new MoviesManiaException("Invalid Cinema Hall ID"));
+		
+		if(show == null)throw new MoviesManiaException("Show is null.");
+
+		boolean check = ch.getShow().stream().anyMatch(s-> {
+			if(s.getDate().equals(show.getDate())) {
+				int startTime = s.getStartTime().getHour()-show.getStartTime().getHour();
+				int endTime = s.getStartTime().getHour()-show.getEndTime().getHour();
+				if((startTime < 0 && endTime > 0) || (startTime > 0 && endTime < 0) ) {
+					return true;
+				}
+			}
+			return false;
+		});
+
+		
+		if(check) {			
+			show.setMovie(movie);
+			show.setCinemaHall(ch);
+			return sr.save(show);
+		}else throw new MoviesManiaException("Cinema hall already booked");
+	}
+
+	@Override
+	public MovieShow mordifyShow(Integer movieId, Integer cinemaHallId, Integer show_id,@Valid MovieShow show) {
+		
+		Movie movie = null;
+		if(movieId != null)movie = mr.findById(movieId).orElseThrow(()-> new MoviesManiaException("Invalid movie ID"));
+		
+		CinemaHall ch = null;
+		if(cinemaHallId != null) ch = chr.findById(cinemaHallId).orElseThrow(()-> new MoviesManiaException("Invalid Cinema Hall ID"));
+		
+		MovieShow movieShow = sr.findById(show_id).orElseThrow(()-> new MoviesManiaException("Invalid Show ID"));
+		
 		if(show == null)throw new MoviesManiaException("Show is null.");
 		
-		show.setMovie(movie);
-		movie.getShowsList().add(show);
+		boolean check = ch.getShow().stream().anyMatch(s-> {
+			if(s.getDate().equals(show.getDate())) {
+				int startTime = s.getStartTime().getHour()-show.getStartTime().getHour();
+				int endTime = s.getStartTime().getHour()-show.getEndTime().getHour();
+				if((startTime < 0 && endTime > 0) || (startTime > 0 && endTime < 0) ) {
+					return true;
+				}
+			}
+			return false;
+		});
 		
-		return sr.save(show);
+		if(movieId != null)show.setMovie(movie);
+		if(cinemaHallId != null)show.setCinemaHall(ch);
+		if(show.getDate() != null)movieShow.setDate(show.getDate());
+		if(show.getStartTime() != null)movieShow.setStartTime(show.getStartTime());
+		if(show.getEndTime() != null)movieShow.setEndTime(show.getEndTime());
+		
+		return sr.save(movieShow);
 	}
 
 }
